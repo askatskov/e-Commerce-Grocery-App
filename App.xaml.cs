53a6875eb5de35633d5975e3f3fb56438e0ef77e
opilane <opilane@HP-CZC0359NNY>
2025-03-03 13:09:00 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace e_Commerce_Grocery_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
